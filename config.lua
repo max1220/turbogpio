520da84f4cf344ad38703710663a6d5deeb05260
max1220 <max1220@users.noreply.github.com>
2016-03-31 03:57:24 +0200

    Removed too many pins

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 return {
 
     -- Pins you want to controll. Adjust to your needs
-    pins = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+    pins = { 3 },
 
     -- Port the webserver binds to.
     webport = 8080,
